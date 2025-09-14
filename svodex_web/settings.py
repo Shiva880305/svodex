475e5de16fe8eb843b3996747bc4df38e351b449
Shiva880305 <marecekluk5@gmail.com>
2025-09-14 22:38:10 +0200

    Removing vercel.json and updating settings for automatic config

--- svodex_web/settings.py
+++ svodex_web/settings.py
@@ -75,7 +75,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # Ujisti se, že tato cesta je správná
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
