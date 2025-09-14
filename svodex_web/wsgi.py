33be620b50666156458f9c3cca4e53f57115b246
Shiva880305 <marecekluk5@gmail.com>
2025-09-14 21:53:37 +0200

    Fix: Add 'app' variable to wsgi.py for Vercel deployment

--- svodex_web/wsgi.py
+++ svodex_web/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for svodex_web project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+# Přidej cestu k adresáři projektu do Python path
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+# Nastav Django settings modul
+os.environ['DJANGO_SETTINGS_MODULE'] = 'svodex_web.settings'
+
+# Importuj a získej WSGI aplikaci
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'svodex_web.settings')
-
 application = get_wsgi_application()
+
+# Tento řádek je klíčový pro nasazení na Vercel
+app = application
